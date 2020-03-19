6020ada892fcdc029ab62856c37f8439ce0847ab
Adam Priest <sepone229@gmail.com>
2020-03-19 10:12:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,9 @@ class School
   
   def add_student(name, grade)
     if @roster[grade] != nil
-      @roster[grade] << @name
+      @roster[grade] << name
     else
-      @roster[grade] = [@name]
+      @roster[grade] = [nname]
     end
   end
   
