edffbc9a63b2f5251c199e3f68595e100f4bcc84
Adam Priest <sepone229@gmail.com>
2020-03-19 09:57:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,6 @@
 require 'pry'
 class School
   
-  def initialize=(school)
-    @school = school
-    @roster = {}
-  end
-  
-  def roster
-    @roster
-  end
-  
-  def add_student=(student, grade)
-    if @roster != nil
-      @roster << students[grade]
-    end
-    
-  end
-    
-  
+  def initialize
 
 end
